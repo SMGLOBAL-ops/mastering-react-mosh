50384744435403a5a593eeffb649d85b733ed822
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-04-21 01:42:49 +0100

    completed up to chapter 4 vid 8 Counters

--- chapter_4/src/components/counter.jsx
+++ chapter_4/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = () => {
@@ -10,17 +10,20 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props); 
-
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button 
+        onClick={() => this.props.onDelete(this.props.counter.id)}
+         className="btn btn-danger btn-sm m-2"
+         >
+           Delete
         </button>
       </div>
     );
@@ -35,10 +38,16 @@ class Counter extends Component {
   formatCount() {
     const { value } = this.state;
     return value === 0 ? "Zero" : value;
-  }
+    }
+
 } 
 
 export default Counter;
+
+
+
+
+
 
 
 
