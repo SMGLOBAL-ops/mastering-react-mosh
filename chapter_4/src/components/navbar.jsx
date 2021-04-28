1b9d955a509f6f7a8522bcf542a45df9f0061666
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-04-28 08:50:53 +0100

    completed up to video 4 Pagination

--- chapter_4/src/components/navbar.jsx
+++ chapter_4/src/components/navbar.jsx
@@ -2,13 +2,13 @@
 
 
 // Stateless Functional Component
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
     return (
         <nav className="navbar navbar-light bg-light">
           <a className="navbar-brand" href="#!">
               Navbar{" "}
               <span className="badge badge-pill badge-secondary">
-                  {props.totalCounters}
+                  {totalCounters}
               </span>
           </a>
         </nav>
@@ -16,3 +16,5 @@ const NavBar = (props) => {
 }; 
 
 export default NavBar;
+
+
